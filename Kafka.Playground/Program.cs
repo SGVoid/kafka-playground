8559d72d7b8d2d0c817ce23f7978d95e97d367a0
serhii.trush <serhii.trush@eleks.com>
2019-03-14 23:02:59 +0200

    Add project files.

---
+++ Kafka.Playground/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kafka.Playground
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
